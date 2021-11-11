1f0560b2cc4edee9b146df500a50ae72fe95eb1b
tungnn-1192 <nguyen.nhat.tung@sun-asterisk.com>
2021-11-11 22:01:53 +0700

    added switching locale feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+  protect_from_forgery with: :exception
+  def default_url_options
+    {locale: I18n.default_locale}
+  end
+  private
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
