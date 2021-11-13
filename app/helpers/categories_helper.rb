77e9e75d821f65c1cca2147e73f77ad3a520b5d7
tungnn-1192 <nguyen.nhat.tung@sun-asterisk.com>
2021-11-13 17:19:36 +0700

    updated mixitup featured products

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -8,13 +8,25 @@ module CategoriesHelper
   end
 
   def featured_products max = 12
-    Product.order(updated_at: :desc).limit(max)
+    Product.order(rating: :desc).limit(max)
+  end
+
+  def to_filters_hash products
+    products.each_with_object({}) do |e, h|
+      cat = e.category
+      # h[cat.id] = cat.title
+      h[cat.parent.id] = cat.parent.title unless cat.parent.nil?
+    end
   end
 
   def snake_join strings
     return "" if strings.empty?
 
-    strings[0] = Strings::Case.snakecase(strings[0])
-    strings.reduce{|a, e| a + " #{Strings::Case.snakecase(e)}"}
+    # strings[0] = Strings::Case.snakecase(strings[0])
+    strings.reduce(""){|a, e| a + " #{to_snakecase e}"}
+  end
+
+  def to_snakecase string
+    Strings::Case.snakecase string
   end
 end
