848527bad6c116bf48920a224c97dfc4740d2ec8
tungnn-1192 <nguyen.nhat.tung@sun-asterisk.com>
2021-11-13 16:49:07 +0700

    updated

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem "rails-i18n"
 gem "bcrypt"
 gem "config"
 gem "breadcrumbs_on_rails"
+gem "strings-case"
 
 group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -6,4 +6,15 @@ module CategoriesHelper
   def categories_carousel max = 10
     Category.limit(max)
   end
+
+  def featured_products max = 12
+    Product.order(updated_at: :desc).limit(max)
+  end
+
+  def snake_join strings
+    return "" if strings.empty?
+
+    strings[0] = Strings::Case.snakecase(strings[0])
+    strings.reduce{|a, e| a + " #{Strings::Case.snakecase(e)}"}
+  end
 end
