21a5664ed3a5d1398df8b2c2da474b0fc02fcf80
tungnn-1192 <nguyen.nhat.tung@sun-asterisk.com>
2021-11-13 11:36:08 +0700

    fixed style for categories carousel in "index" page, a bit in "cart_index" page, accidently fixed code style in "session_helper"

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     base_title = t "app_name"
     page_title.empty? ? base_title : "#{page_title} | #{base_title}"
   end
+
+  def print_price price
+    number_with_delimiter price, delimiter: "."
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,7 +11,7 @@ module SessionsHelper
     session[:cart][3.to_s] = 4
     session[:cart][4.to_s] = 3
 
-    return self.cart_contents
+    cart_contents
   end
 
   def cart_count
@@ -29,10 +29,10 @@ module SessionsHelper
     # retrive product info by id (pair.key = item id, pair.value = item quantity)
     products_contents = {}
     @products.each do |pair|
-      products_contents[pair] = Product.find_by_id(pair[0])
+      products_contents[pair] = Product.find_by(id: pair[0])
     end
 
-    return products_contents
+    products_contents
   end
 
   def cart_add_item
@@ -50,36 +50,12 @@ module SessionsHelper
   def subtotal
     @products = cart_contents
     subtotal = 0
-    unless @products.blank?
+    if @products.present?
       @products.each do |pair|
-        subtotal += pair[1][:price].to_f * pair[0][1].to_f
+        subtotal += pair[1][:price] * pair[0][1]
       end
     end
 
-    return subtotal.round(2)
+    subtotal.round(2)
   end
-
-  # def build_order
-  #   @products = cart_contents
-  #   order = []
-  #   @products.each do |product|
-  #     order << {name: product[0].name,
-  #       quantity: product[1]["qty"],
-  #       amount: product[0].price.to_i}
-  #     end
-
-  #     return order
-  #   end
-
-  #   def build_json
-  #     cart_session = session[:cart][:products]
-  #     json = {:subtotal => self.subtotal.to_f.round(2),
-  #       :qty => self.cart_count,
-  #       :items => Hash[cart_session.uniq.map {
-  #         |i| [i, cart_session.count(i)]
-  #       }]}
-
-  #       return json
-  #     end
-
-    end
+end
