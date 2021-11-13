c45240c9e8a097199072d07e18cb64ecc6f08668
tungnn-1192 <nguyen.nhat.tung@sun-asterisk.com>
2021-11-13 15:49:11 +0700

    hotfix changing locale error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :set_locale, :fake_cart, :add_index_breadcrumb
   protect_from_forgery with: :exception
   def default_url_options
-    {locale: I18n.default_locale}
+    {locale: I18n.locale}
   end
   private
   def set_locale
