b0b11f851233c7a07c8f5b7e9b0c8fee0cbad810
laipmv-1193 <pham.mai.van.lai@sun-asterisk.com>
2021-11-24 13:22:15 +0700

    Cart Update Quantity

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,21 @@
 class CartsController < ApplicationController
   def index; end
+
+  def create
+    @result = cart_handle_add
+    respond_to do |format|
+      format.js {}
+    end
+  end
+
+  def update
+    @result = cart_handle_remove
+    respond_to do |format|
+      format.js {}
+    end
+  end
+
+  def destroy
+    remove_item
+  end
 end
