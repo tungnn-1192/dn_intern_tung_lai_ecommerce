7287154beafc19bb3c23e92ac9f2c3f18d37e842
tungnn-1192 <nguyen.nhat.tung@sun-asterisk.com>
2021-11-26 16:32:21 +0700

    Added admin show order page, added update order status

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,11 +1,31 @@
 class Admin::OrdersController < Admin::BaseController
   before_action :load_orders_paginate, only: :index
+  before_action :load_order, only: %i(show update)
 
   def index; end
+
+  def show; end
+
+  def update
+    if @order.update_attribute :status, Order.statuses.key(params[:status].to_i)
+      flash[:success] = t("admin.status_updated")
+    else
+      flash[:danger] = t("admin.status_update_failed")
+    end
+    redirect_to admin_orders_url
+  end
 
   private
 
   def load_orders_paginate
     @orders = Order.page params[:page]
+  end
+
+  def load_order
+    @order = Order.find_by id: params[:id]
+    return if @order.present?
+
+    flash[:warning] = t("admin.order_not_found", id: params[:id])
+    redirect_to admin_orders_url
   end
 end
