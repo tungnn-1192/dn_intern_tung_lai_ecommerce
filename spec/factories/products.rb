59094e567a4626e47fdfec35c98883f818f80c11
tungnn-1192 <nguyen.nhat.tung@sun-asterisk.com>
2021-12-03 13:46:32 +0700

    Added RSpec for Product model

---
+++ spec/factories/products.rb
@@ -0,0 +1,48 @@
+FactoryBot.define do
+  factory :product do
+    name{Faker::Commerce.product_name}
+    description{product_description}
+    price{product_price}
+    rating{product_rating}
+    inventory{product_inventory}
+    unit{Settings.seeder.unit}
+    category{create(:category)}
+
+    trait :with_parent_category do
+      transient do
+        parent_cat{nil}
+      end
+
+      after(:create) do |invoice, evaluator|
+        invoice.category.parent = evaluator.parent_cat
+        # create :line_item, invoice: invoice, amount: evaluator.amount
+      end
+    end
+  end
+end
+
+private
+
+def product_description
+  Faker::Lorem.paragraphs(
+    number: Settings.seeder.description_paragraphs
+  ).join(". ")
+end
+
+def product_price
+  Faker::Number.number(digits: Settings.seeder.digit.price)
+end
+
+def product_rating
+  Faker::Number.between(
+    from: Settings.seeder.digit.rating.min,
+    to: Settings.seeder.digit.rating.max
+  )
+end
+
+def product_inventory
+  Faker::Number.between(
+    from: Settings.seeder.digit.inventory.min,
+    to: Settings.seeder.digit.inventory.max
+  )
+end
